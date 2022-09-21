eecfe67968bfbced7ad33a6edbe6df794278cba1
rizwanweb <rizwansoomro@zoho.com>
2022-09-22 01:52:24 +0500

    password changed functionality done

--- account/urls.py
+++ account/urls.py
@@ -6,8 +6,13 @@ urlpatterns = [
     # Function Based Login View
     #path('login', views.user_login, name='login'),
 
+    # login and logout urls
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('', views.dashboard, name='dashboard')
+    # Change Password Urls
+    path('password_change', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('', views.dashboard, name='dashboard'),
 ]
